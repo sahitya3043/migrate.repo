72ed06dc6063e9797ec10ee3cd356e98d38821f2
dev2 <dev2@mail.com>
2019-07-09 07:39:29 -0400

    helo file modf by dev2

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-class hello   # from dev1
+class hello   #from dev1
 {
-	public static void main()  # from dev1
+	public static void main()  #from dev1
 	{
 		system out prittn("hello");  from dev1
 	}
