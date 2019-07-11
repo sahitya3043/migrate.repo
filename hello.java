923e577b06e889e5e0c6c768287be7ef44a1096a
dev1 <dev1@mail.com>
2019-07-11 19:49:24 +0530

    helo file conflict resolved

--- hello.java
+++ hello.java
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
+
 class hello.java    #from dev2
 {
 	public static void main() #from dev2
-=======
-class hello.java   #from dev1
+
 {
-	public static void main()   #from dev1
->>>>>>> 940397a732bb227a7587411ad1d946ade2e77336
-	{
 		system out println ("hello");     #from dev1
 	}
 }    #from dev2
